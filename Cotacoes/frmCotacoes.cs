981fcbef78aab99d189f9bdef07bb442f356e4f2
Nelson Frick <nfrick@gmail.com>
2017-09-22 19:28:36 -0300

    Cotacoes - Contas picklist ordered.

--- Cotacoes/frmCotacoes.cs
+++ Cotacoes/frmCotacoes.cs
@@ -24,7 +24,7 @@ namespace Cotacoes {
 
             var cbx = toolStripComboBoxConta.ComboBox;
             using (var ctx = new InvestimentosEntities()) {
-                cbx.Items.AddRange(ctx.Contas.ToArray());
+                cbx.Items.AddRange(ctx.Contas.OrderBy(c => c.Nome).ToArray());
             }
             cbx.DisplayMember = "Nome";
             cbx.ValueMember = "ContaId";
